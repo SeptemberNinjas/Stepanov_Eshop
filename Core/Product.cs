4dcad214d32f1e44460b6e449ea9e13f734084e3
Дмитрий Степанов <ofluft@gmail.com>
2024-10-06 12:00:22 +1000

    добавление классов продукта, услуги и категории.

---
+++ Core/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Core
+{
+    public class Product
+    {
+        private int _id { get => _id; set => _id = value; }
+        private string _name { get => _name; set => _name = value; }
+        private double _price { get => _price; set => _price = value; } 
+        private int _stock { get => _stock; set => _stock = value; }
+        private ProductCategory _category { get => _category; set => _category = value; }
+
+        public Product(int id, string name, double price, int stock, ProductCategory category)
+        {
+            _id = id;
+            _name = name;
+            _price = price;
+            _stock = stock;
+            _category = category;
+        }
+    }
+}
---
+++ Core/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class ProductCategory
+    {
+        private int _id { get => _id; set => _id = value; }
+        private string _name { get => _name; set => _name = value; }
+        public ProductCategory(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+
+    }
+}
